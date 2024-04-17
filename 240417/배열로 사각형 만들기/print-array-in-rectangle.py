a7ec93937201700ac1111416868f9edfee70297f
jongha kim <jjhsun99@gmail.com>
2024-04-17 23:47:32 +0900

    Title: 배열로 사각형 만들기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240417/배열로 사각형 만들기/print-array-in-rectangle.py
@@ -0,0 +1,22 @@
+n =5
+
+arr_2d=[
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+for j in range(n):
+    arr_2d[0][j] = 1
+
+for i in range(n):
+    arr_2d[i][0] = 1
+
+
+for i in range(1,5):
+    for j in range(1,5):
+        arr_2d[i][j] = arr_2d[i-1][j] + arr_2d[i][j - 1]
+
+for row in arr_2d:
+    for elem in row:
+        print(elem, end=" ")
+    print()
