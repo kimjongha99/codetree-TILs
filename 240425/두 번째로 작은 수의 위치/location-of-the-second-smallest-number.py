1bd66cf35857c65aeb392a3d98d617e0a90e1ac9
jongha kim <jjhsun99@gmail.com>
2024-04-25 20:27:53 +0900

    Title: 두 번째로 작은 수의 위치, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240425/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
+++ 240425/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
@@ -1,33 +1,32 @@
 import sys
 
-tc = int(input())
+# 변수 선언 및 입력
+n = int(input())
+a = list(map(int, input().split()))
 
+# 새로운 배열을 만들어 정렬하고
+# 2번째로 작은 숫자를 찾아냅니다.
+myarr = sorted(a)
 
-arr= list(map(int,input().split()))
-
-myarr = sorted(arr)
-
-
+# isexist : 2번째로 작은 숫자가 존재하면 true
 isexist = False
-
 low2 = 0
-
-
 for elem in myarr:
-    if elem != arr[0]:
+    # 가장 처음으로 myarr[0]과 다른 숫자는
+    # 2번째로 작은 숫자라고 할 수 있습니다.
+    if elem != myarr[0]:
         low2 = elem
         isexist = True
         break
 
+# 2번째로 작은 숫자가 존재하지 않을 때
 if isexist == False:
     print(-1)
     sys.exit()
 
-
 ansidx = -1
-
-for idx , elem  in enumerate(arr):
-  if elem == low2:
+for idx, elem in enumerate(a):
+    if elem == low2:
         # 2번째로 작은 숫자가 여러 개 있을 때
         if ansidx != -1:
             print(-1)
