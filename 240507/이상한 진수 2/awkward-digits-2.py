fe10cd209a97be0c7e191b751cb87b83d75c8bdb
jongha kim <jjhsun99@gmail.com>
2024-05-07 15:31:53 +0900

    Title: 이상한 진수 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240507/이상한 진수 2/awkward-digits-2.py
+++ 240507/이상한 진수 2/awkward-digits-2.py
@@ -1,15 +1,17 @@
 binary_string = input()
+if binary_string == "1":
+    # 입력이 "1"인 경우는 "0"으로 바꿔서 출력
+    max_value = 0
+else:
+    # 첫 번째 '0'을 '1'로 변경하여 최댓값을 찾기
+    max_binary = binary_string
+    for i in range(len(binary_string)):
+        if binary_string[i] == '0':
+            max_binary = binary_string[:i] + '1' + binary_string[i+1:]
+            break
 
-max_binary = binary_string
-for i in range(len(binary_string)):
-    if(len(binary_string)==0):
-        print(0)
-        break
-    if binary_string[i] == '0':
-        max_binary = binary_string[:i] + '1' + binary_string[i+1:]
-        break
-# 변경된 2진수 문자열을 10진수로 변환
-max_value = int(max_binary, 2)
+    # 변경된 2진수 문자열을 10진수로 변환
+    max_value = int(max_binary, 2)
 
 # 결과 출력
 print(max_value)
