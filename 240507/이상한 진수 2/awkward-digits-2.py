d3f39cb4a8221bf2003ca2defc6f8db8c4102165
jongha kim <jjhsun99@gmail.com>
2024-05-07 15:11:31 +0900

    Title: 이상한 진수 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240507/이상한 진수 2/awkward-digits-2.py
@@ -0,0 +1,12 @@
+binary_string = input()
+
+max_binary = binary_string
+for i in range(len(binary_string)):
+    if binary_string[i] == '0':
+        max_binary = binary_string[:i] + '1' + binary_string[i+1:]
+        break
+# 변경된 2진수 문자열을 10진수로 변환
+max_value = int(max_binary, 2)
+
+# 결과 출력
+print(max_value)
