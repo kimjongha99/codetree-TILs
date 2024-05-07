a5f96bcc2a02d5d3f12814160b48b761db79294c
jongha kim <jjhsun99@gmail.com>
2024-05-07 16:34:06 +0900

    Title: Date to Date, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 240507/Date to Date/date-to-date.py
+++ 240507/Date to Date/date-to-date.py
@@ -3,7 +3,7 @@ start_m , start_d , end_m, end_d = map(int,input().split())
 
 num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-sum_day=0
+sum_day=1
 
 
 if start_m == end_m and start_d == end_d:
@@ -17,7 +17,7 @@ else:
         sum_day+=1
 
         if start_d > num_of_days[start_m]:
-            start_d=0
+            start_d = 1  # 날짜를 1로 설정하여 다음 달의 첫째 날로 설정
             start_m+=1
 
     print(sum_day)
