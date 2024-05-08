a3952f09c9987dcc95743f62da0832c84bba2e38
jongha kim <jjhsun99@gmail.com>
2024-05-08 11:13:55 +0900

    Title: DateTime to DateTime, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240508/DateTime to DateTime/datetime-to-datetime.py
+++ 240508/DateTime to DateTime/datetime-to-datetime.py
@@ -1,32 +1,11 @@
-def calculate_minutes(a, b, c):
-    # Starting time: November 11, 11:11
-    start_d, start_h, start_m = 11, 11, 11
+# 변수 선언 및 입력
+a, b, c = tuple(map(int, input().split()))
 
-    # End time provided by the input
-    end_d, end_h, end_m = a, b, c
+# 차이를 계산합니다.
+diff = (a * 24 * 60 + b * 60 + c) - (11 * 24 * 60 + 11 * 60 + 11)
 
-    # If the given end time is before the start time, return -1
-    if (end_d < start_d) or (end_d == start_d and end_h < start_h) or (end_d == start_d and end_h == start_h and end_m < start_m):
-        return -1
-
-    # Calculate the total minutes between the two times
-    minutes_from_start_to_midnight = (23 - start_h) * 60 + (59 - start_m) + 1
-    minutes_from_midnight_to_end = end_h * 60 + end_m
-    days_between = (end_d - start_d - 1) * 24 * 60
-
-    # Total minutes calculation
-    if end_d == start_d:
-        # Same day calculation
-        total_minutes = (end_h - start_h) * 60 + (end_m - start_m)
-    else:
-        # Different days calculation
-        total_minutes = minutes_from_start_to_midnight + days_between + minutes_from_midnight_to_end
-
-    return total_minutes
-
-# Input
-a, b, c = map(int, input().split())
-
-# Calculate and print result
-result = calculate_minutes(a, b, c)
-print(result)
+# 출력
+if diff < 0:
+    print(-1)
+else:
+    print(diff)
