66f3b695160d7aabd36c7e92461abef1c3bc1252
jongha kim <jjhsun99@gmail.com>
2024-05-09 19:32:18 +0900

    Title: 그 요일은, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240509/그 요일은/the-day-of-the-day.py
+++ 240509/그 요일은/the-day-of-the-day.py
@@ -1,42 +1,35 @@
-# Receives m1, d1, m2, d2 as input
+#  m1 ,d1 , m2 , d2 를 입력받음
 m1, d1, m2, d2 = tuple(map(int, input().split()))
 
-# Enter the day to be counted
-target_day = input()
+# 날짜를 입력받음
+yoil = input()
 
-# Days in each month for a leap year 2024
-days_in_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-# Days of the week starting from Monday (since Jan 1, 2024 is a Monday)
-week_days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-# Calculate starting weekday index
-def calculate_starting_weekday(m, d):
-    days = sum(days_in_month[:m]) + d - 1
-    return week_days[days % 7]  # Jan 1 is Monday, no offset needed for 2024
+# 1월부터 12월의 날짜 일수를 배열에 저장  [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 2월 윤년
+month=[0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+date = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 
-# Count occurrences of target_day from (m1, d1) to (m2, d2)
-def count_target_day_occurrences(m1, d1, m2, d2, target_day):
-    # Find starting weekday for m1, d1
-    start_day = calculate_starting_weekday(m1, d1)
-    start_index = week_days.index(start_day)
-    target_index = week_days.index(target_day)
 
-    # Calculate the total number of days between the two dates
-    total_days = sum(days_in_month[m1:m2]) + d2 - d1 + 1
-    if m1 == m2:
-        total_days = d2 - d1 + 1
+ans =1
 
-    # Adjust if the start month is the same as the end month
-    current_day_index = start_index
-    count = 0
-    for _ in range(total_days):
-        if week_days[current_day_index] == target_day:
-            count += 1
-        current_day_index = (current_day_index + 1) % 7
+while True:
 
-    return count
+    if m1 == m2 and d1 == d2:
+        break
 
-# Calculate and print the result
-result = count_target_day_occurrences(m1, d1, m2, d2, target_day)
-print(result)
+
+
+    d1 +=1
+
+    val = date[d1%7]
+
+    if val == yoil:
+        ans+=1
+
+
+    if d1 == month[m1]:
+        d1 = 0
+        m1 +=1
+
+print(ans)
