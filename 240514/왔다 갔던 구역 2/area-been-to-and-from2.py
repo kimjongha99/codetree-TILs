e9234286189398f638f895944ca88f0c41b0ab6a
jongha kim <jjhsun99@gmail.com>
2024-05-14 14:47:57 +0900

    Title: 왔다 갔던 구역 2, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

--- 240514/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240514/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 # Initialize an array to track the number of times each position is passed
-# We use 201 to cover the range from -100 to 100
+# We use 2001 to cover the range from -1000 to 1000
 arr = [0] * 2001
 
 # The starting point, which represents position 0
@@ -16,7 +16,7 @@ def move_right(x):
 
 def move_left(x):
     global point
-    for j in range(point, point - x, -1):
+    for j in range(point - 1, point - x - 1, -1):
         arr[j] += 1
     point -= x
 
