6ba83efb9c50e1eb8b878213c89ca6a8bac7c1a6
jongha kim <jjhsun99@gmail.com>
2024-05-14 11:40:56 +0900

    Title: 최대로 겹치는 지점, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

--- 240514/최대로 겹치는 지점/maximum-overlapped-points.py
+++ 240514/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -1,11 +1,11 @@
 n = int(input())
 
 
-arr = [0] * 201
+arr = [0] * 101
 
 for i in range(n):
     x,y = map(int,input().split())
-    for j in range(x + 100, y + 100):
+    for j in range(x , y+1):
         arr[j] += 1
 
 print (max(arr))
