e4118283dc8c4f4762278d1f4a4567fe404015cb
jongha kim <jjhsun99@gmail.com>
2024-05-17 22:22:34 +0900

    Title: 오름 내림차순 정렬, Time: 219ms, Memory: 24MB, Status: Passed - CodeTree

--- 240517/오름 내림차순 정렬/inc-dec-sorting.py
+++ 240517/오름 내림차순 정렬/inc-dec-sorting.py
@@ -2,15 +2,15 @@ n = int(input())
 
 arr = list(map(int,input().split()))
 
-
-desc_arr = sorted(arr, key= lambda x:x , reverse= True)
-
-for elem in desc_arr:
-    print(elem, end=" ")
-
-
-print()
 asc_arr = sorted(arr, key= lambda x:x , reverse= False)
 
 for elem in asc_arr:
+    print(elem, end=" ")
+
+
+
+print()
+desc_arr = sorted(arr, key= lambda x:x , reverse= True)
+
+for elem in desc_arr:
     print(elem, end=" ")
