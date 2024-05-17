4cac34abf3355ab864e0b79b12994d38626c2978
jongha kim <jjhsun99@gmail.com>
2024-05-17 15:16:24 +0900

    Title: 특별한 문자, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240517/특별한 문자/special-character.py
@@ -0,0 +1,19 @@
+word = input()
+
+di ={}
+
+for char in word:
+    if char in di:
+        di[char] +=1
+    else:
+        di[char] =1
+
+di_list = list(di.items())
+
+di_list.sort(key=lambda x:x[1],reverse=False)
+
+
+if di_list[0][1] >=1 :
+    print("None")
+else:
+    print(di_list[0][0])
