f721eed5dfb446c43a1a6779520b2c730ac96f9d
jongha kim <jjhsun99@gmail.com>
2024-05-17 11:43:33 +0900

    Title: 두 수의 합, Time: 5052ms, Memory: 34MB, Status: Time Limit Exceed - CodeTree

---
+++ 240517/두 수의 합/sum-of-two-num.py
@@ -0,0 +1,13 @@
+n, k = map(int,input().split())
+
+arr = list(map(int,input().split()))
+
+cnt =0
+
+for i in range(n):
+
+    for j in range(i+1,n):
+        if k == arr[i]+arr[j]:
+            cnt+=1
+
+print(cnt)
