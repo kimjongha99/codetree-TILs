d752a3273537b8157d900e4dca5d67f36cf9ed46
jongha kim <jjhsun99@gmail.com>
2024-05-18 22:07:05 +0900

    Title: 국영수 순이지, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240518/국영수 순이지/korean-english-math-order.py
+++ 240518/국영수 순이지/korean-english-math-order.py
@@ -4,10 +4,12 @@ arr = []
 
 for _ in range(n):
     name, subject1,subject2,subject3  = input().split()
+    subject1, subject2, subject3 = int(subject1), int(subject2), int(subject3)
+
     arr2 = [name, subject1,subject2,subject3]
     arr.append(arr2)
 
-sort_arr = sorted(arr,key= lambda x:(x[0], x[1], x[2] ), reverse= True)
+sort_arr = sorted(arr,key= lambda x:(x[1], x[2], x[3] ), reverse= True)
 
 for name, subject1,subject2,subject3 in sort_arr:
     print(name, subject1,subject2,subject3)
