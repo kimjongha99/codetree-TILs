7cfcfefa1315a549691feef886fc223a7321b671
jongha kim <jjhsun99@gmail.com>
2024-05-26 18:59:46 +0900

    Title: 두 개의 격자 비교하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240526/두 개의 격자 비교하기/compare-two-grid.py
@@ -0,0 +1,36 @@
+n , m = map(int,input().split())
+
+arr1 =[
+    list(map(int,input().split()))
+
+    for _ in range(n)
+
+]
+
+arr2 = [
+    list(map(int, input().split()))
+
+    for _ in range(n)
+
+]
+
+check_arr =[
+    [0 for _ in range(n)]
+
+    for _ in range(m)
+]
+
+
+
+for i in range(n):
+    for j in range(m):
+        if arr1[i][j]==arr2[i][j]:
+            check_arr[i][j]=0
+        else:
+            check_arr[i][j]=1
+
+
+for row in check_arr:
+    for ele in row:
+        print(ele , end=" ")
+    print()
