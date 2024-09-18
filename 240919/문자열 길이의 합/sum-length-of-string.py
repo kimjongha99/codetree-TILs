9e72e8dd7c419f75acd24b66d794b9b345ef00fa
jongha kim <jjhsun99@gmail.com>
2024-09-19 00:54:55 +0900

    Title: 문자열 길이의 합, Time: 97ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240919/문자열 길이의 합/sum-length-of-string.py
@@ -0,0 +1,19 @@
+n = int(input())
+arr1 =[
+]
+
+for _ in range(n):
+    str1 = input()
+    arr1.append(str1)
+    
+
+count =0
+count2 =0
+for ele in arr1:
+    for ele1 in ele:
+        count +=1
+        if ele1 == 'a':
+            count2+=1
+
+
+print(count, count2)
