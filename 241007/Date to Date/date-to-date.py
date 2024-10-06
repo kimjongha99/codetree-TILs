4da0dd410aa54b92f223496e8d7f4d156183347b
jongha kim <jjhsun99@gmail.com>
2024-10-07 01:32:38 +0900

    Title: Date to Date, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241007/Date to Date/date-to-date.py
@@ -0,0 +1,21 @@
+month, day , end_m , end_d = map(int,input().split())
+
+elapsed_days = 1
+#                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+
+while True:
+    if month == end_m and day ==end_d: 
+        break
+    
+
+    elapsed_days +=1
+    day +=1
+
+    if day >num_of_days[month]:
+        month+=1
+        day = 1
+
+print(elapsed_days)
